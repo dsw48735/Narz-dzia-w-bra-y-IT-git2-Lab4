843207c9ffd072beb8fb3691ba2f8c0ea9617c6e
Teodor Harlender <teodormaxharlender@interia.pl>
2023-07-01 00:31:24 +0200

    m4

--- skrypt.sh
+++ skrypt.sh
@@ -13,3 +13,12 @@ elif [ "$1" == "--logs" ]; then
     filename="log${i}.txt"
     echo -e "Nazwa pliku: $filename\nNazwa skryptu: $0\nData: $(date)" > $filename
   done
+elif [ "$1" == "--help" ]; then
+  echo "Dostępne opcje:"
+  echo "- --date: Wyświetla dzisiejszą datę"
+  echo "- --logs: Tworzy automatycznie 100 plików logx.txt z informacjami"
+  echo "- --logs <liczba>: Tworzy automatycznie <liczba> plików logx.txt z informacjami"
+  echo "- --help: Wyświetla wszystkie dostępne opcje"
+else
+  echo "Nieznana opcja. Użyj '--help' aby zobaczyć dostępne opcje."
+fi
